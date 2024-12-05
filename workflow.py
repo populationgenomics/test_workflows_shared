589a151ade5bbbcd0a7f9832b6618dad895089e0
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-12-06 10:15:11 +1100

    initial commit

---
+++ workflow.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import os
+import sys
+from pathlib import Path
+
+from cpg_flow.workflow import run_workflow
+from cpg_utils.config import set_config_paths
+
+TMP_DIR = os.getenv('TMP_DIR')
+CONFIG_FILE = str(Path(__file__).parent / 'config.toml')
+
+message = "Hello, Hail Batch! I'm CPG flow, nice to meet you."
+
+
+def run_cpg_flow(dry_run=False):
+    from stages import CumulativeCalc, GeneratePrimes
+
+    workflow = [GeneratePrimes, CumulativeCalc]
+
+    config_paths = os.environ['CPG_CONFIG_PATH'].split(',')
+
+    # Inserting after the "defaults" config, but before user configs:
+    set_config_paths(config_paths[:1] + [CONFIG_FILE] + config_paths[1:])
+    run_workflow(stages=workflow, dry_run=dry_run)
+
+
+def validate_batch_workflow():
+    if not os.path.exists(f'{TMP_DIR}/out.txt'):
+        print('Batch workflow failed')
+        sys.exit(1)
+
+    success = False
+    with open(f'{TMP_DIR}/out.txt', 'r') as f:
+        success = f.read().strip() == message
+
+    print(f'Batch workflow {"succeeded" if success else "failed"}')
+    sys.exit(0 if success else 1)
+
+
+if __name__ == '__main__':
+    run_cpg_flow()
