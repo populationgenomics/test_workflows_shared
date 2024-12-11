79ff4736894be64d443662767bedfc6e03dcc0a6
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-12-11 14:28:55 +1100

    wip: testing build pyramid

--- run-test-workflow.sh
+++ run-test-workflow.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Check for unstaged changes in the git repo
+if [[ -n $(git status -s) ]]; then
+  echo "There are unstaged changes in the git repo. Please commit or stash them before running this script."
+  exit 1
+fi
+
+# Check for uncommitted changes in the git repo
+if [[ -n $(git diff) ]]; then
+  echo "There are uncommitted changes in the git repo. Please commit or stash them before running this script."
+  exit 1
+fi
 
 echo analysis-runner \
     --image "australia-southeast1-docker.pkg.dev/cpg-common/images/cpg_flow:0.1.0-alpha.9" \
