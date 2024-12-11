eeafcab3d3fe76e2214401a979d76d6d6d75531f
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-12-11 15:35:02 +1100

    wip: testing build pyramid

--- jobs/build_pyramid.py
+++ jobs/build_pyramid.py
@@ -3,30 +3,28 @@ from typing import Any
 from cpg_flow.targets.sequencing_group import SequencingGroup
 from hailtop.batch import Batch
 from hailtop.batch.job import Job
-from hailtop.batch.resource import ResourceFile
 
 
 def build_pyramid(
     b: Batch,
     sequencing_groups: list[SequencingGroup],
     input_files: dict[str, Any],
-    output_file_path: ResourceFile,
+    output_file_path: str,
 ) -> list[Job]:
-    title = 'Filter Evens'
-    job = b.new_job(name=title)
-
+    title = 'Build A Pyramid'
     # Compute the no evens list for each sequencing group
+    sg_jobs = []
     sg_output_files = []
     for sg in sequencing_groups:  # type: ignore
+        job = b.new_job(name=title + ': ' + sg.id)
         no_evens_input_file_path = input_files[sg.id]['no_evens']
-        id_sum_input_file_path = input_files[sg.id]['id_sum']
-
         no_evens_input_file = b.read_input(no_evens_input_file_path)
+
+        id_sum_input_file_path = input_files[sg.id]['id_sum']
         id_sum_input_file = b.read_input(id_sum_input_file_path)
 
-        pyramid_output_file_path = sg.dataset.prefix() / f'{sg.id}_pyramid.txt'
+        pyramid_output_file_path = str(sg.dataset.prefix() / f'{sg.id}_pyramid.txt')
         sg_output_files.append(pyramid_output_file_path)
-
         cmd = f"""
             pyramid=()
             max_row_size=(cat {no_evens_input_file} | rev | cut -d' ' -f1 | rev)
@@ -46,14 +44,17 @@ def build_pyramid(
         """
 
         job.command(cmd)
-        b.write_output(job.pyramid_file, pyramid_output_file_path)
+        b.write_output(job.sg_no_evens_file, pyramid_output_file_path)
+        sg_jobs.append(job)
 
     # Merge the no evens lists for all sequencing groups into a single file
+    job = b.new_job(name=title)
+    job.depends_on(*sg_jobs)
     inputs = ' '.join([b.read_input(f) for f in sg_output_files])
-    job.command(f'cat {inputs} >> {job.pyramid_file}')
+    job.command(f'cat {inputs} >> {job.pyramid}')
+    b.write_output(job.pyramid, output_file_path)
 
-    print('-----PRINT NO EVENS-----')
+    print('-----PRINT PYRAMID-----')
     print(output_file_path)
-    b.write_output(job.pyramid_file, output_file_path)
 
     return job
