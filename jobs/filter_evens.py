14ecef9f24283c1f4b04c6bc5c501a41dedea8d8
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2024-12-11 11:53:24 +1100

    test(jobs/filter_evens.py): re-write to make multiple jobs

--- jobs/filter_evens.py
+++ jobs/filter_evens.py
@@ -13,11 +13,12 @@ def filter_evens(
     output_file_path: ResourceFile,
 ) -> list[Job]:
     title = 'Filter Evens'
-    job = b.new_job(name=title)
 
     # Compute the no evens list for each sequencing group
+    sg_jobs = []
     sg_output_files = []
     for sg in sequencing_groups:  # type: ignore
+        job = b.new_job(name=title + ': ' + sg.id)
         input_file_path = input_files[sg.id]['cumulative']
         no_evens_input_file = b.read_input(input_file_path)
         no_evens_output_file_path = str(sg.dataset.prefix() / f'{sg.id}_no_evens.txt')
@@ -31,18 +32,21 @@ def filter_evens(
                 result+=("$num")
             fi
         done
-        echo "{sg.id}: ${{result[@]}}" > {job.no_evens_sg_file}
+        echo "{sg.id}: ${{result[@]}}" > {job.sg_no_evens_file}
         """
 
         job.command(cmd)
-        b.write_output(job.no_evens_sg_file, no_evens_output_file_path)
+        b.write_output(job.sg_no_evens_file, no_evens_output_file_path)
+        sg_jobs.append(job)
 
     # Merge the no evens lists for all sequencing groups into a single file
+    job = b.new_job(name=title)
+    job.depends_on(sg_jobs)
     inputs = ' '.join([b.read_input(f) for f in sg_output_files])
     job.command(f'cat {inputs} >> {job.no_evens_file}')
+    b.write_output(job.no_evens_file, output_file_path)
 
     print('-----PRINT NO EVENS-----')
     print(output_file_path)
-    b.write_output(job.no_evens_file, output_file_path)
 
     return job
