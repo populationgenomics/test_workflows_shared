68ee0775fca39237064c4d8e4683438f62b959e7
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2024-12-13 11:33:45 +1100

    feat(run-test-workflow.sh): allow image tag to be passed in

--- run-test-workflow.sh
+++ run-test-workflow.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+DEFAULT_IMAGE_TAG="0.1.0-alpha.9"
+
+# If there is a command line argument, use it as the image tag
+if [[ -n $1 ]]; then
+  echo "Using image tag: $1"
+  IMAGE_TAG=$1
+else
+  echo "Using default image tag: $DEFAULT_IMAGE_TAG"
+  IMAGE_TAG=$DEFAULT_IMAGE_TAG
+fi
+
 # Check for unstaged changes in the git repo
 if [[ -n $(git status -s) ]]; then
   echo "There are unstaged changes in the git repo. Please commit or stash them before running this script."
@@ -11,17 +23,17 @@ if [[ -n $(git diff) ]]; then
   exit 1
 fi
 
-echo analysis-runner \
-    --image "australia-southeast1-docker.pkg.dev/cpg-common/images/cpg_flow:0.1.0-alpha.9" \
-    --dataset "fewgenomes" \
-    --description "cpg-flow_test" \
-    --access-level "test" \
-    --output-dir "cpg-flow_test" \
-    --config "config.toml" \
+echo analysis-runner \\
+    --image "australia-southeast1-docker.pkg.dev/cpg-common/images/cpg_flow:$DEFAULT_IMAGE_TAG" \\
+    --dataset "fewgenomes" \\
+    --description "cpg-flow_test" \\
+    --access-level "test" \\
+    --output-dir "cpg-flow_test" \\
+    --config "config.toml" \\
     workflow.py
 
 analysis-runner \
-    --image "australia-southeast1-docker.pkg.dev/cpg-common/images/cpg_flow:0.1.0-alpha.9" \
+    --image "australia-southeast1-docker.pkg.dev/cpg-common/images/cpg_flow:$DEFAULT_IMAGE_TAG" \
     --dataset "fewgenomes" \
     --description "cpg-flow_test" \
     --access-level "test" \
