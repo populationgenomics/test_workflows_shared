f70c6a4708d8851454c8b404a543fede7c9dc3e1
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2025-01-21 14:55:07 +1100

    feat(cpg_flow_test/,cpg_flow_test/jobs/): add new job to test workflow

---
+++ cpg_flow_test/jobs/say_hi.py
@@ -0,0 +1,24 @@
+from cpg_flow.targets.sequencing_group import SequencingGroup
+from hailtop.batch import Batch
+from hailtop.batch.job import Job
+
+
+def say_hi(
+    b: Batch,
+    sequencing_group: SequencingGroup,
+    output_file_path: str,
+) -> list[Job]:
+    title = f'Say Hi: {sequencing_group.id}'
+    job = b.new_job(name=title)
+
+    cmd = f"""
+    echo "This is a hello from sequencing_group {sequencing_group.id}" > {job.sayhi}
+    """
+
+    job.command(cmd)
+
+    print('-----PRINT SAY HI-----')
+    print(output_file_path)
+    b.write_output(job.sayhi, output_file_path)
+
+    return job
